3f30f3c739f1b3bdfb28fd789572e49033a5799f
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2021-01-14 16:46:05 -0500

    completed main objective. getting an error in the console, but scatter plot is functioning as it should

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -43,7 +43,7 @@ d3.csv("/assets/data/data.csv")
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    // Append axis to the chart
+    // Append axes to the chart
     chartGroup.append("g")
       .attr("transform", `translate(0, ${height})`)
       .call(bottomAxis);
@@ -78,3 +78,30 @@ d3.csv("/assets/data/data.csv")
          toolTip.hide(data)
       d3.select(this).style("stroke","#e3e3e3")
      });
+
+    // Initialize tool tip
+    var toolTip = d3.tip()
+      .attr("class", "d3-tip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}<br>poverty: ${d.poverty}<br>healthcare: ${d.healthcare}`);
+      });
+
+    // Create tooltip in the chart
+    chartGroup.call(toolTip);
+    chartGroup.on('mouseover', function(d) { toolTip.show(d, this); })
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Health Care(%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Poverty (%)");
+  });
