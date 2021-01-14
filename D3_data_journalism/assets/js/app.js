c0b9c2cd88b8cde542ac33213586e2bfed9601f2
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2021-01-14 15:30:35 -0500

    parsed through data, created scale and axis functions and appended the axis to the chart

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -12,11 +12,41 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3
-  .select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
-  .attr("height", svgHeight)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("height", svgHeight);
 
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Get data from csv
+d3.csv("/assets/data/data.csv")
+  .then(function(healthdata) {
+
+    // Parse data/cast as numbers
+    healthdata.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
+
+    // Create scale functions
+    var xLinearScale = d3.scaleLinear()
+      .domain([8, d3.max(healthdata, d => d.poverty*1.2)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([2, d3.max(healthdata, d => d.healthcare*1.2)])
+      .range([height, 0]);
+
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append axis to the chart
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
